bc6b3f8d4d0a5d1780f18b72515a5cbf9f093eca
Cuneyt Oghan <coghann@gmail.com>
2023-02-03 19:52:31 +0000

    create vpc and subnets w/ IGW

---
+++ task1/vpc.tf
@@ -0,0 +1,38 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "taskvpc"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "taskIG"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "subnet1"
+  }
+}
+resource "aws_subnet" "subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "subnet2"
+  }
+}
+resource "aws_subnet" "subnet3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "subnet3"
+  }
+}
