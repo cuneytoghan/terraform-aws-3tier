a2bc6a4e502b4f34069175b5774155bb72181cba
Cuneyt Oghan <coghann@gmail.com>
2023-02-04 19:25:52 +0000

    assign public ip to the subnets

--- task1/vpc.tf
+++ task1/vpc.tf
@@ -22,6 +22,8 @@ resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  
   tags = {
     Name = "subnet1"
   }
@@ -30,6 +32,8 @@ resource "aws_subnet" "subnet2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
   tags = {
     Name = "subnet2"
   }
@@ -38,6 +42,8 @@ resource "aws_subnet" "subnet3" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+
   tags = {
     Name = "subnet3"
   }
