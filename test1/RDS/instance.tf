04692313f8ef4b5d8d69d8ec3b31cee9f9349fab
Cuneyt Oghan <coghann@gmail.com>
2023-02-06 07:14:28 +0000

    update instance.tf

--- test1/RDS/instance.tf
+++ test1/RDS/instance.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "web1" {
   instance_type = "t2.micro"
   key_name      = "admin_key"
 
-  depends_on = [aws_db_instance.my_db]
+  depends_on = [aws_key_pair.admin_key, aws_db_instance.my_db]
   tags = merge(
     var.common_tags,
     {
