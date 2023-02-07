cd43f4cfc557725b9e7a4d35692bb6f2a4cf5cf0
Cuneyt Oghan <coghann@gmail.com>
2023-02-07 05:01:13 +0000

    instance sg playbook.yml

---
+++ task1/example-provisioner/instance.tf
@@ -0,0 +1,16 @@
+resource "aws_key_pair" "bastion_key" {
+  key_name   = "bastion-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "myinstance1" {
+  ami             = "ami-0b5eea76982371e91"
+  instance_type   = "t2.micro"
+  security_groups = ["ssh-sg", "http-sg"]
+
+  # provisioner "local-exec" {
+  #   command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i \"${self.public_ip},\" playbook.yml -u ec2-user -b"
+  # }
+
+  key_name = "bastion-key"
+}
